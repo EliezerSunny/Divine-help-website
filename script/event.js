89969a4b9ba43b86f4881be9b0ae43a3cc609181
EliezerSunny <adetunjieliazer@gmail.com>
2024-12-22 03:41:50 +0100

    modified

--- script/event.js
+++ script/event.js
@@ -11,7 +11,7 @@ let year = date.getFullYear();
 let currentDate = day + "-" + month + "-" + year;
 
 
-if (month == "12" || month == "1") {
+if (day >= "December 15" && day <= "January 15") {
     document.getElementById("greetings").innerHTML = "Merry Christmas & Happy New Year!!! &#127881;&#x1F389; 🎇";
 } else if (month == "11") {
     document.getElementById("greetings").innerHTML = "Anniversary! LOADING... &#127881;&#x1F389;";
